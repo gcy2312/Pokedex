660308417e2ea1567eaf8db493af9ebc70882b8f
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-03-12 19:19:05 -0500

    js weedle

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,26 @@ let pokemonList [
   weight: 6.9,
   type: ["grass", "poison"],
   abilities: ["chlorophyll", "overegrow"]
-  }
+  },
 
   {name: "Charmander",
   height: 0.6,
   weight: 8.5,
   type: "fire",
   abilities:["blaze", "solar-power"]
-  }
+  },
 
   {name: "Squirtle",
   height: 0.5,
   weight: 9,
   type: "water",
   abilities:["rain-dish", "torrent"]
-  }
+  },
+
+  {name: "Weedle",
+  height: 0.3,
+  weight: 3.2,
+  type: ["bug", "poison"],
+  abilities: ["sheild-dust", "run-away"]
+  },
 ]
