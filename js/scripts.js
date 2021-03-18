36b085db80caafc8693a7ac07b48e6e64ccb4d92
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-03-18 18:35:36 -0400

    change loop for forEach

--- js/scripts.js
+++ js/scripts.js
@@ -81,14 +81,14 @@ let pokemonList=[
 ]
 
 //loop to display pokemon names and height
-for (let i= 0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + " " + '(height: ' + pokemonList[i].height + ')')
+function myListFunction(pokemon){
+  document.write("<p>" + pokemon.name + " " + '(height: ' + pokemon.height + ')');
 
-  //conditional big and small additional text
-  if(pokemonList[i].height > 1.5){
-    document.write(' Wow, that\'s a BIG pokemon!')
-  }else if(pokemonList[i].height < 0.4){
-    document.write(' Awww, so cute and tiny!')
+  if (pokemon.height > 1.5){
+        document.write(' Wow, that\'s a BIG pokemon!' + "</p>")
+  }else if (pokemon.height < 0.4){
+        document.write(' Awww, so cute and tiny!' + "</p>")
   }
-  document.write("<br>")
-}
+};
+
+pokemonList.forEach(myListFunction);
