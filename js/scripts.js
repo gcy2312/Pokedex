b13eba170c5b4daa47e05886fd8a21979c13750a
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-03-24 12:46:36 -0400

    loadList function

--- js/scripts.js
+++ js/scripts.js
@@ -41,6 +41,23 @@ let pokemonRepository = (function(){
     console.log(pokemon.name);
   };
 
+  function loadList(){
+    return fetch(apiUrl).then (function(response){
+      return response.json();
+    }).then (function (json){
+      json.results.forEach(function(item){
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function(e){
+      console.error(e);
+    })
+  }
+
+
   return{
     add: add,
     getAll: getAll,
